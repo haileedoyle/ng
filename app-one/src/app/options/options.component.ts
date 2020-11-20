8148cd473fa21ceaed2a9d825f45a5161ee07828
Hailee Doyle <haileexdoyle@gmail.com>
2020-11-20 14:50:27 -0700

    option bug

--- app-one/src/app/options/options.component.ts
+++ app-one/src/app/options/options.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Option } from './option';
 
+
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html',
   styleUrls: ['./options.component.css']
 })
 export class OptionsComponent implements OnInit {
-  option: Option = {
-    id: 1,
-    name: 'About Us'
-  }
+ option = [
+    { id: 1, name: 'Home'},
+    { id: 2, name: 'Offerings'},
+    { id: 3, name: 'Us'},
+    { id: 4, name: 'Contact'}
+  ]
 
     constructor() { }
 
